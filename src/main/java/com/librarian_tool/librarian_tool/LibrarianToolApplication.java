bb86c073ac099a107dd8c55b9d85942cb4c67602
root <root@DESKTOP-R4J0G6A.localdomain>
2024-11-13 10:57:22 +0000

    chore: initialize plain project

---
+++ src/main/java/com/librarian_tool/librarian_tool/LibrarianToolApplication.java
@@ -0,0 +1,13 @@
+package com.librarian_tool.librarian_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarianToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarianToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/librarian_tool/librarian_tool/LibrarianToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarian_tool.librarian_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarianToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
